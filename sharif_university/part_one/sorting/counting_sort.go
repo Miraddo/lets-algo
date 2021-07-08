0c8b6fe50635b55ec244ee2919c275f3f8cbf466
miraddo <mposhtdari@gmail.com>
2021-07-08 20:46:26 +0430

    Counting Sort Algorithm Updated (added comments)

--- sharif_university/part_one/sorting/counting_sort.go
+++ sharif_university/part_one/sorting/counting_sort.go
@@ -1,11 +1,12 @@
 package Sorting
 
+// CountingSort Counting Sort Algorithm
 func CountingSort(nums []int) []int{
 
 	var max int
 	ln := len(nums)
 	j := 1
-
+	// find max number inside array
 	for i := range nums{
 		if nums[i] > nums[j+1]{
 			j++
@@ -13,23 +14,23 @@ func CountingSort(nums []int) []int{
 		}
 	}
 
+	// create an array with max+1 size
 	count := make([]int, max+1)
-
+	// with x value we add 1 to index x inside count array
 	for _, x := range nums{
 		count[x] += 1
 	}
 
+	// now we looking for each key and each key has one or more value
 	j = 1
-
 	for i:=1; i< len(count) - 1; i++{
 
 		count[j+1] = count[i] + count[j+1]
 		j++
 	}
 
-
+	// then we create another array with the same size as nums
 	result := make([]int, ln)
-
 	for _, x :=range nums{
 
 		result[count[x]-1] = x
