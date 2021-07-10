3335624b60e419075dbbd3028afe6c231aeed338
miraddo <mposhtdari@gmail.com>
2021-07-10 23:38:28 +0430

    Bucket Sort Algorithm

---
+++ sharif_university/part_one/sorting/bucket_sort.go
@@ -0,0 +1,35 @@
+package Sorting
+
+import (
+	"math"
+)
+
+//BucketSort Bucket Sort Algorithm base on
+func BucketSort(nums []int) []int {
+
+	ln := len(nums)
+	var result []int
+	var storeList [][]int = make([][]int, ln)
+
+	// put numbers into buckets
+	for _, x := range nums {
+
+		j := int(math.Floor(float64(x / 10)))
+
+		storeList[j] = append(storeList[j], x)
+	}
+
+	// sort each array with bubble sort algorithm or other algorithm that we have
+	for _, x := range storeList {
+		if len(x) > 1 {
+			list := BubbleSort(x)
+			for _, i := range list{
+				// save result
+				result = append(result, i)
+			}
+
+		}
+	}
+
+	return result
+}
