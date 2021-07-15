810e32be909129af417d8b0784a50ffa58cd6df6
Milad <mposhtdari@gmail.com>
2021-07-14 21:21:01 -0700

    LeetCode - Easy - Remove Element

---
+++ leetcode/easy/27.RemoveElement/main.go
@@ -0,0 +1,14 @@
+package main
+
+// removeElement based on : https://leetcode.com/problems/remove-element
+func removeElement(nums []int, val int) int {
+	var i int
+	for _ , x := range nums{
+		if x != val{
+			nums[i] = x
+			i++
+		}
+	}
+
+	return i
+}
