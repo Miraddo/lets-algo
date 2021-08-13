039a737b3a1d961b7e2cb174854925a464ea5a37
Milad <mposhtdari@gmail.com>
2021-08-13 13:04:59 +0430

    better way using binary search in LeetCode No.34

--- leetcode/medium/34.FindFirstAndLastPositionOfElementInSortedArray/main.go
+++ leetcode/medium/34.FindFirstAndLastPositionOfElementInSortedArray/main.go
@@ -6,40 +6,73 @@ import (
 // 34. Find First and Last Position of Element in Sorted Array
 // https://leetcode.com/problems/find-first-and-last-position-of-element-in-sorted-array/
 
+// this way is better it should be O(log n)
 func searchRange(nums []int, target int) []int {
-	var result []int
-	var lastIndex int
-	firstIndex := LinearSearch(nums, target)
-	if firstIndex == -1{
-		return []int{-1, -1}
-	}
 
-	lastIndex = findLastIndex(nums[firstIndex:], target)
+	first := binarySearch(nums, target, true)
+	last := binarySearch(nums, target, false)
 
-	result = append(result, firstIndex)
-	result = append(result, firstIndex + lastIndex)
-
-	return  result
+	return []int{first, last}
 }
 
-func findLastIndex(last []int, target int) int {
-	var res int
-	for i, x := range last{
-		if x == target{
-			res = i
+func binarySearch(nums []int, target int, pn bool) int {
+	left, right := 0, len(nums) -1
+	res := -1
+	for left <= right{
+		mid := (left + right) / 2
+
+		if target > nums[mid]{
+			left = mid + 1
+		}else if target < nums[mid]{
+			right = mid -1
+		}else{
+			res = mid
+			if pn {
+				right = mid -1
+			}else {
+				left = mid + 1
+			}
 		}
 	}
 	return res
 }
 
-func LinearSearch(nums []int, target int) int {
-	for i, x := range nums {
-		if x == target {
-			return i
-		}
-	}
-	return -1
-}
+
+// is is take O(n) is not what we want
+//func searchRange(nums []int, target int) []int {
+//	var result []int
+//	var lastIndex int
+//	firstIndex := LinearSearch(nums, target)
+//	if firstIndex == -1{
+//		return []int{-1, -1}
+//	}
+//
+//	lastIndex = findLastIndex(nums[firstIndex:], target)
+//
+//	result = append(result, firstIndex)
+//	result = append(result, firstIndex + lastIndex)
+//
+//	return  result
+//}
+//
+//func findLastIndex(last []int, target int) int {
+//	var res int
+//	for i, x := range last{
+//		if x == target{
+//			res = i
+//		}
+//	}
+//	return res
+//}
+//
+//func LinearSearch(nums []int, target int) int {
+//	for i, x := range nums {
+//		if x == target {
+//			return i
+//		}
+//	}
+//	return -1
+//}
 
 func main()  {
 	fmt.Println(searchRange([]int{5,7,7,8,8,10}, 8))
